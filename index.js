0cfae6e040b36c942993896e9086bea864b39798
Michael Desanker <mdesanker@gmail.com>
2021-10-29 15:59:29 -0400

    refactor: convert to async/await function

--- index.js
+++ index.js
@@ -7,17 +7,31 @@ const input = document.querySelector("#search");
 const catBtn = document.querySelector(".cat-btn");
 
 // Functions
-const getGif = function (search) {
-  fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=cJnK6ceWr5Y9KevmRJ3uP19FYsdv6yW6&s=${search}`,
-    { mode: "cors" }
-  )
-    .then((res) => res.json())
-    .then((pdata) => (image.src = pdata.data.images.original.url))
-    .catch((err) => {
-      console.error(err);
-      getGif("404");
-    });
+// const getGif = function (search) {
+//   fetch(
+//     `https://api.giphy.com/v1/gifs/translate?api_key=cJnK6ceWr5Y9KevmRJ3uP19FYsdv6yW6&s=${search}`,
+//     { mode: "cors" }
+//   )
+//     .then((res) => res.json())
+//     .then((pdata) => (image.src = pdata.data.images.original.url))
+//     .catch((err) => {
+//       console.error(err);
+//       getGif("404");
+//     });
+// };
+
+// Refactor as async/await function
+const getGif = async function (search) {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=cJnK6ceWr5Y9KevmRJ3uP19FYsdv6yW6&s=${search}`,
+      { mode: "cors" }
+    );
+    const gifData = await response.json();
+    image.src = gifData.data.images.original.url;
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 const clearForm = function () {
@@ -31,11 +45,11 @@ getGif("cat");
 catBtn.addEventListener("click", (e) => {
   e.preventDefault();
   getGif("cat");
-  clearForm();
+  // clearForm();
 });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   getGif(input.value);
-  clearForm();
+  // clearForm();
 });
