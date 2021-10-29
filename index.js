04a8e9ef977d905506cc1c814a2aefd54c5305db
Michael Desanker <mdesanker@gmail.com>
2021-10-29 14:00:03 -0400

    feat: add new gif button

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 "use strict";
 
 const image = document.querySelector("img");
+const newGifBtn = document.querySelector(".new");
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=cJnK6ceWr5Y9KevmRJ3uP19FYsdv6yW6&s=cats",
-  { mode: "cors" }
-)
-  .then((res) => res.json())
-  .then((data) => (image.src = data.data.images.original.url));
+const newCatGif = function () {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=cJnK6ceWr5Y9KevmRJ3uP19FYsdv6yW6&s=cats",
+    { mode: "cors" }
+  )
+    .then((res) => res.json())
+    .then((data) => (image.src = data.data.images.original.url));
+};
+
+newCatGif();
+
+newGifBtn.addEventListener("click", newCatGif);
